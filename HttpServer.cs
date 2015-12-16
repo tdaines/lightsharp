42540e9606690ab9b59e7dc0daaa046ab06a2891
tdaines <tdaines@gmail.com>
2015-12-15 21:46:12 -0800

    Got basic http server working using sample from https://github.com/ms-iot/samples/tree/develop/App2App%20WebServer

---
+++ HttpServer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using Windows.Foundation.Collections;
+using Windows.Networking.Sockets;
+using System.IO;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace LightSharp
+{
+    internal class HttpServer : IDisposable
+    {
+        string offHtmlString = "<html><head><title>Blinky App</title></head><body><form action=\"blinky.html\" method=\"GET\"><input type=\"radio\" name=\"state\" value=\"on\" onclick=\"this.form.submit()\"> On<br><input type=\"radio\" name=\"state\" value=\"off\" checked onclick=\"this.form.submit()\"> Off</form></body></html>";
+        string onHtmlString = "<html><head><title>Blinky App</title></head><body><form action=\"blinky.html\" method=\"GET\"><input type=\"radio\" name=\"state\" value=\"on\" checked onclick=\"this.form.submit()\"> On<br><input type=\"radio\" name=\"state\" value=\"off\" onclick=\"this.form.submit()\"> Off</form></body></html>";
+
+        private const uint BUFFER_SIZE = 8192;
+        private int port;
+        private readonly StreamSocketListener listener;
+
+        public HttpServer(int port)
+        {
+            Debug.WriteLine("HttpServer::HttpServer({0})", port);
+
+            this.port = port;
+            listener = new StreamSocketListener();
+            listener.ConnectionReceived += ConnectionReceived;
+        }
+
+        public void StartServerAsync()
+        {
+            Debug.WriteLine("HttpServer::StartServerAsync()");
+
+#pragma warning disable CS4014
+            listener.BindServiceNameAsync(port.ToString());
+#pragma warning restore CS4014
+        }
+
+        public void Dispose()
+        {
+            Debug.WriteLine("HttpServer::Dispose()");
+
+            listener.ConnectionReceived -= ConnectionReceived;
+            listener.Dispose();
+        }
+
+        private async void ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            Debug.WriteLine("HttpServer::ConnectionReceived()");
+
+            // this works for text only
+            StringBuilder request = new StringBuilder();
+            using (IInputStream input = args.Socket.InputStream)
+            {
+                byte[] data = new byte[BUFFER_SIZE];
+                IBuffer buffer = data.AsBuffer();
+                uint dataRead = BUFFER_SIZE;
+                while (dataRead == BUFFER_SIZE)
+                {
+                    await input.ReadAsync(buffer, BUFFER_SIZE, InputStreamOptions.Partial);
+                    request.Append(Encoding.UTF8.GetString(data, 0, data.Length));
+                    dataRead = buffer.Length;
+                }
+            }
+
+            using (IOutputStream output = args.Socket.OutputStream)
+            {
+                string requestMethod = request.ToString().Split('\n')[0];
+                string[] requestParts = requestMethod.Split(' ');
+
+                if (requestParts[0] == "GET")
+                    await WriteResponseAsync(requestParts[1], output);
+                else
+                    throw new InvalidDataException("HTTP method not supported: "
+                                                   + requestParts[0]);
+            }
+        }
+
+        private async Task WriteResponseAsync(string request, IOutputStream os)
+        {
+            // See if the request is for blinky.html, if yes get the new state
+            string state = "Unspecified";
+//            bool stateChanged = false;
+            if (request.Contains("blinky.html?state=on"))
+            {
+                state = "On";
+//                stateChanged = true;
+            }
+            else if (request.Contains("blinky.html?state=off"))
+            {
+                state = "Off";
+//                stateChanged = true;
+            }
+
+//            if (stateChanged)
+//            {
+//                var updateMessage = new ValueSet();
+//                updateMessage.Add("State", state);
+//                var responseStatus = await appServiceConnection.SendMessageAsync(updateMessage);
+//            }
+
+            string html = state == "On" ? onHtmlString : offHtmlString;
+            // Show the html 
+            using (Stream resp = os.AsStreamForWrite())
+            {
+                // Look in the Data subdirectory of the app package
+                byte[] bodyArray = Encoding.UTF8.GetBytes(html);
+                MemoryStream stream = new MemoryStream(bodyArray);
+                string header = string.Format("HTTP/1.1 200 OK\r\n" +
+                                  "Content-Length: {0}\r\n" +
+                                  "Connection: close\r\n\r\n",
+                                  stream.Length);
+                byte[] headerArray = Encoding.UTF8.GetBytes(header);
+                await resp.WriteAsync(headerArray, 0, headerArray.Length);
+                await stream.CopyToAsync(resp);
+                await resp.FlushAsync();
+            }
+
+        }
+    }
+}
---
+++ HttpServerTask.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+using Windows.System.Threading;
+
+namespace LightSharp
+{
+    public sealed class HttpServerTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral serviceDeferral;
+        private HttpServer httpServer;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debug.WriteLine("HttpServerTask::Run()");
+
+            // Associate a cancellation handler with the background task. 
+            taskInstance.Canceled += OnCanceled;
+
+            // Get the deferral object from the task instance
+            serviceDeferral = taskInstance.GetDeferral();
+
+            httpServer = new HttpServer(8000);
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            ThreadPool.RunAsync(w => httpServer.StartServerAsync());
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            Debug.WriteLine("HttpServerTask::OnCanceled()");
+
+            httpServer.Dispose();
+            httpServer = null;
+
+            serviceDeferral.Complete();
+        }
+    }
+}
